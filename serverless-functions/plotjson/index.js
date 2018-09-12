5d727e914d746f999bcfd0eee855d5a671e61058
Donald Pinckney <donald_pinckney@icloud.com>
2018-09-12 16:57:26 -0400

    Reorganize stuff to remove functions.json

--- serverless-functions/plotjson/index.js
+++ serverless-functions/plotjson/index.js
@@ -1,7 +1,22 @@
 'use strict';
 
-const Functions = require('./functions');
+const exec = require('child_process').exec;
 
-exports.plotjson = (req, res) => {
-  res.send(Functions.plotjson(req.body, req.query));
+
+/**
+ *
+ * @param {Object} req A Javascript object of the JSON body, or null if there is no parsable JSON body
+ * @param {Object} res A Javascript object of the GET query.
+ */
+function plotjson(jsonBody, getQuery, callback) {
+    exec('ls /usr/bin', {stdio: 'ignore'}, (err, stdout) => {
+        callback(stdout);
+    });
+    // callback(`Hello ${jsonBody.name || 'World'}!`);
+}
+
+exports.plotjson_GCF = (req, res) => {
+  plotjson(req.body, req.query, output => {
+    res.send(output)
+  });
 };
