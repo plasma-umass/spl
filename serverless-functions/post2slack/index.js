90fd27c324bf37030aedc46bcfa7128497eee6a9
sp1tz <42953063+sp1tz@users.noreply.github.com>
2018-09-12 16:04:18 -0400

    Fix whitespace.

--- serverless-functions/post2slack/index.js
+++ serverless-functions/post2slack/index.js
@@ -19,19 +19,19 @@ exports.main = function(req, res) {
   }, function(response) {//One-time listener for the response event.
     var payload = [];
 
-	  response.on('error', function(e) {//Error making call to Slack API.
-	    res.status(500).json({
-	    	error: `An error occured while attempting to post to Slack; ${e}.` 
-	    });
-	  }).on('data', function(chunk) {//Receiving...
-	    payload.push(chunk);
-	  }).on('end', function() {//Done; response received in full.
-	    payload = JSON.parse(String(Buffer.concat(payload)));
-	    res.status(payload.ok ? 200 : 500).json({
-	    	info: 'POST attempt complete; see included results from Slack.',
-	    	slack: payload
-	    });
-	  });
+    response.on('error', function(e) {//Error making call to Slack API.
+      res.status(500).json({
+          error: `An error occured while attempting to post to Slack; ${e}.`
+      });
+    }).on('data', function(chunk) {//Receiving...
+      payload.push(chunk);
+    }).on('end', function() {//Done; response received in full.
+      payload = JSON.parse(String(Buffer.concat(payload)));
+      res.status(payload.ok ? 200 : 500).json({
+          info: 'POST attempt complete; see included results from Slack.',
+          slack: payload
+      });
+    });
   });
 
   request.write(req.body);
