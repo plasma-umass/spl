54ecf7cff22c6b6fc4979c14ab648de2e0e006cc
Donald Pinckney <donald_pinckney@icloud.com>
2018-09-18 22:04:01 -0400

    Modify JSON transformer language to implicitely convert an array to an object with keys "0", "1", etc.

--- spl-lib/src/json_transformers/eval.rs
+++ spl-lib/src/json_transformers/eval.rs
@@ -1,13 +1,34 @@
-use serde_json::Value;
+use serde_json::{Value, Map};
 extern crate serde_json;
 use super::syntax::*;
+
+fn to_object_and_convert_array(v: &Value) -> Option<Box<Map<String, Value>>> {
+    use std::iter::FromIterator;
+
+    match v {
+        Value::Object(map) => Some(Box::new(map.to_owned())),
+        Value::Array(vector) => {
+            let mut m = Map::new();
+
+            for i in 0..vector.len() {
+                m.insert(i.to_string(), vector[i].clone());
+            }
+            Some(Box::new(m))
+        }
+        _ => None
+    }
+}
 
 fn eval_pat(pat: &Pat, value: &Value) -> Option<Value> {
     match pat {
         Pat::Empty => Option::Some(value.clone()),
-        Pat::Pat(PatAtom::Select(key), p) => value.as_object()
-            .and_then(|map| map.get(key))
-            .and_then(|v| eval_pat(p, v)),
+        Pat::Pat(PatAtom::Select(key), p) => {
+            if let Some(map_val) = to_object_and_convert_array(value) {
+                map_val.get(key).and_then(|v| eval_pat(p, v))
+            } else {
+                None
+            }
+        },
         Pat::Pat(PatAtom::Map(f), p) => match value {
             Value::Array(vec) => vec.iter()
                 .map(|e| eval(f, e))
