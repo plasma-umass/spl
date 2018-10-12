4239f1b04fddfe2231b77d48a4133cf2158a4d1b
sp1tz <42953063+sp1tz@users.noreply.github.com>
2018-10-12 00:42:46 -0400

    Initial setup of CDS.

--- serverless-functions/payday/index.js
+++ serverless-functions/payday/index.js
@@ -1,5 +1,14 @@
 'use strict';
+const Datastore = require('@google-cloud/datastore');
 
 exports.main = function(req, res) {
-  res.send(req.body);
+  const datastore = new Datastore({
+    projectId: 'umass-plasma'
+  });
+
+  const key = datastore.key(['Account', 'Alice']);
+  datastore.get(key, function(err, entity) {
+    console.log(entity);
+    res.sendStatus(err ? 500 : 200);
+  });
 };
