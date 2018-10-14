e7e5b82d96bc6f19ed7ca23bd10abd74820d5b8f
sp1tz <42953063+sp1tz@users.noreply.github.com>
2018-10-14 18:11:32 -0400

    Remove more error handling; other small tweaks for paper.

--- serverless-functions/payday/index.js
+++ serverless-functions/payday/index.js
@@ -16,16 +16,12 @@ exports.main = function(req, res) {
           let acctNum = dsClient.key(['Account', req.body.to]);
 
           dsClient.get(acctNum, function(err, acct) {
-            if(err || !acct) {
-              dsClientTrans.rollback(function() { res.send('Retrieve account failed.'); });
-            } else {
-              acct.Balance += req.body.amount;
-              dsClient.update({ key: acctNum, data: acct }, function() {
-                dsClient.insert({ key: transId, data: {} }, function() {
-                  dsClientTrans.commit(function() { res.send('Deposit complete.'); });
-                });
+            acct.Balance += req.body.amount;
+            dsClient.update({ key: acctNum, data: acct }, function() {
+              dsClient.insert({ key: transId, data: {} }, function() {
+                dsClientTrans.commit(function() { res.send('Deposit complete.'); });
               });
-            }
+            });
           });
         } else if(req.body.type === 'transfer') {
           let amnt = req.body.amount;
@@ -33,24 +29,23 @@ exports.main = function(req, res) {
           let acctNumTo = dsClient.key(['Account', req.body.to]);
 
           dsClient.get([acctNumFrom, acctNumTo], function(err, accts) {
-            if(err || !accts[0] || !accts[1]) {
-              dsClientTrans.rollback(function() { res.send('Retrieve accounts failed.'); });
-            } else {
-              if(accts[0].Balance >= amnt) {
-                accts[0].Balance -= amnt; accts[1].Balance += amnt;
-                dsClient.update([{ key: acctNumFrom, data: accts[0] },
-                  { key: acctNumTo, data: accts[1] }], function() {
-                  dsClient.insert({ key: transId, data: {} }, function() {
-                    dsClientTrans.commit(function() { res.send('Transfer complete.'); });
-                  });
+            if(accts[0].Balance >= amnt) {
+              accts[0].Balance -= amnt;
+              accts[1].Balance += amnt;
+              dsClient.update([{ key: acctNumFrom, data: accts[0] },
+                { key: acctNumTo, data: accts[1] }], function() {
+                dsClient.insert({ key: transId, data: {} }, function() {
+                  dsClientTrans.commit(function() { res.send('Transfer complete.'); });
                 });
-              } else {
-                dsClientTrans.rollback(function() { res.send('Insufficient funds.'); });
-              }
+              });
+            } else {
+              dsClientTrans.rollback(function() { res.send('Insufficient funds.'); });
             }
           });
         } else {
-          dsClientTrans.rollback(function() { res.send(`Unknown operation: ${req.body.type}.`); });
+          dsClientTrans.rollback(function() {
+            res.send('Unknown operation: ' + req.body.type);
+          });
         }
       }
     });
