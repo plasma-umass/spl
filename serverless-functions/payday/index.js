3cb59c529631586a9baf77468fd3c647c1bfc8cc
sp1tz <42953063+sp1tz@users.noreply.github.com>
2018-10-14 17:48:41 -0400

    Use 'let' and 'function' KW.

--- serverless-functions/payday/index.js
+++ serverless-functions/payday/index.js
@@ -1,60 +1,60 @@
 'use strict';
-const Datastore = require('@google-cloud/datastore');
+let Datastore = require('@google-cloud/datastore');
 
 exports.main = function(req, res) {
-  const dsClient = new Datastore({ projectId: 'umass-plasma' }),
+  let dsClient = new Datastore({ projectId: 'umass-plasma' }),
     dsClientTrans = dsClient.transaction();
 
-  dsClientTrans.run(() => {
-    const transId = dsClient.key(['Transaction', req.body.transId]);
+  dsClientTrans.run(function() {
+    let transId = dsClient.key(['Transaction', req.body.transId]);
 
-    dsClient.get(transId, (err, trans) => {
+    dsClient.get(transId, function(err, trans) {
       function postTrans(success, failure) {
-        dsClient.insert({ key: transId, data: {} }, (err) => {
-          err ? dsClientTrans.rollback(() => { res.send(failure); }) :
-            dsClientTrans.commit(() => { res.send(success); });
+        dsClient.insert({ key: transId, data: {} }, function(err) {
+          err ? dsClientTrans.rollback(function() { res.send(failure); }) :
+            dsClientTrans.commit(function() { res.send(success); });
         });
       }
 
       if(err || trans) {
-        dsClientTrans.rollback(() => { res.send('Invalid transaction ID.'); });
+        dsClientTrans.rollback(function() { res.send('Invalid transaction ID.'); });
       } else {
         if(req.body.type.trim().toLowerCase() === 'deposit') {
-          const acctNum = dsClient.key(['Account', req.body.to]);
+          let acctNum = dsClient.key(['Account', req.body.to]);
 
-          dsClient.get(acctNum, (err, acct) => {
+          dsClient.get(acctNum, function(err, acct) {
             if(err || !acct) {
-              dsClientTrans.rollback(() => { res.send('Retrieve account failed.'); });
+              dsClientTrans.rollback(function() { res.send('Retrieve account failed.'); });
             } else {
               acct.Balance += req.body.amount;
-              dsClient.update({ key: acctNum, data: acct }, (err) => {
-                err ? dsClientTrans.rollback(() => { res.send('Update account failed.'); }) :
+              dsClient.update({ key: acctNum, data: acct }, function(err) {
+                err ? dsClientTrans.rollback(function() { res.send('Update account failed.'); }) :
                   postTrans('Deposit complete.', 'Update account failed.');
               });
             }
           });
         } else if(req.body.type.trim().toLowerCase() === 'transfer') {
-          const amnt = req.body.amount, acctNumFrom = dsClient.key(['Account', req.body.from]),
+          let amnt = req.body.amount, acctNumFrom = dsClient.key(['Account', req.body.from]),
             acctNumTo = dsClient.key(['Account', req.body.to]);
 
-          dsClient.get([acctNumFrom, acctNumTo], (err, accts) => {
+          dsClient.get([acctNumFrom, acctNumTo], function(err, accts) {
             if(err || !accts[0] || !accts[1]) {
-              dsClientTrans.rollback(() => { res.send('Retrieve accounts failed.'); });
+              dsClientTrans.rollback(function() { res.send('Retrieve accounts failed.'); });
             } else {
               if(accts[0].Balance >= amnt) {
                 accts[0].Balance -= amnt; accts[1].Balance += amnt;
                 dsClient.update([{ key: acctNumFrom, data: accts[0] },
-                  { key: acctNumTo, data: accts[1] }], (err) => {
-                  err ? dsClientTrans.rollback(() => { res.send('Update accounts failed.'); }) :
+                  { key: acctNumTo, data: accts[1] }], function(err) {
+                  err ? dsClientTrans.rollback(function() { res.send('Update accounts failed.'); }) :
                     postTrans('Transfer complete.', 'Update accounts failed.');
                 });
               } else {
-                dsClientTrans.rollback(() => { res.send('Insufficient funds.'); });
+                dsClientTrans.rollback(function() { res.send('Insufficient funds.'); });
               }
             }
           });
         } else {
-          dsClientTrans.rollback(() => { res.send(`Unknown operation: ${req.body.type}.`); });
+          dsClientTrans.rollback(function() { res.send(`Unknown operation: ${req.body.type}.`); });
         }
       }
     });
