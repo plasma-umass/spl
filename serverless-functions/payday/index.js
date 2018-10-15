d4491151f3d25a2ce8a509c4b8103a5f4769bd2c
sp1tz <42953063+sp1tz@users.noreply.github.com>
2018-10-15 12:34:37 -0400

    Use transaction API correctly.

--- serverless-functions/payday/index.js
+++ serverless-functions/payday/index.js
@@ -8,36 +8,32 @@ exports.main = function(req, res) {
   dsClientTrans.run(function() {
     let transId = dsClient.key(['Transaction', req.body.transId]);
 
-    dsClient.get(transId, function(err, trans) {
+    dsClientTrans.get(transId, function(err, trans) {
       if(err || trans) {
         dsClientTrans.rollback(function() { res.send('Invalid transaction ID.'); });
       } else {
         if(req.body.type === 'deposit') {
           let acctNum = dsClient.key(['Account', req.body.to]);
 
-          dsClient.get(acctNum, function(err, acct) {
+          dsClientTrans.get(acctNum, function(err, acct) {
             acct.Balance += req.body.amount;
-            dsClient.update({ key: acctNum, data: acct }, function() {
-              dsClient.insert({ key: transId, data: {} }, function() {
-                dsClientTrans.commit(function() { res.send('Deposit complete.'); });
-              });
-            });
+            dsClientTrans.save({ key: acctNum, data: acct });
+            dsClientTrans.save({ key: transId, data: {} });
+            dsClientTrans.commit(function() { res.send('Deposit complete.'); });
           });
         } else if(req.body.type === 'transfer') {
           let amnt = req.body.amount;
           let acctNumFrom = dsClient.key(['Account', req.body.from]);
           let acctNumTo = dsClient.key(['Account', req.body.to]);
 
-          dsClient.get([acctNumFrom, acctNumTo], function(err, accts) {
+          dsClientTrans.get([acctNumFrom, acctNumTo], function(err, accts) {
             if(accts[0].Balance >= amnt) {
               accts[0].Balance -= amnt;
               accts[1].Balance += amnt;
-              dsClient.update([{ key: acctNumFrom, data: accts[0] },
-                { key: acctNumTo, data: accts[1] }], function() {
-                dsClient.insert({ key: transId, data: {} }, function() {
-                  dsClientTrans.commit(function() { res.send('Transfer complete.'); });
-                });
-              });
+              dsClientTrans.save([{ key: acctNumFrom, data: accts[0] },
+                { key: acctNumTo, data: accts[1] }]);
+              dsClientTrans.save({ key: transId, data: {} });
+              dsClientTrans.commit(function() { res.send('Transfer complete.'); });
             } else {
               dsClientTrans.rollback(function() { res.send('Insufficient funds.'); });
             }
