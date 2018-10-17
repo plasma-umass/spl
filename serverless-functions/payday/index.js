e6cc1330dd81bbafc00b953aa0c12914b20c51b3
Joseph S <42953063+sp1tz@users.noreply.github.com>
2018-10-17 15:05:17 -0400

    Bank Example (#22)
    * Initial commit
    
    * Initial setup of CDS.
    
    * Persistence via GCDS.
    
    * Transactions.
    
    * Exactly once.
    
    * Shorten names.
    
    * Arrow functions, shorten messages, fewer newlines.
    
    * Helper fn; ternary ops.
    
    * Helper fn outsie of block; clean type param.
    
    * Detailed docs.
    
    * Use 'let' and 'function' KW.
    
    * Separate declarations; no trim/lowercase
    
    * Remove helper function; rid of error handling.
    
    * Remove more error handling; other small tweaks for paper.
    
    * Use transaction API correctly.

---
+++ serverless-functions/payday/index.js
@@ -0,0 +1,49 @@
+'use strict';
+let Datastore = require('@google-cloud/datastore');
+
+exports.main = function(req, res) {
+  let dsClient = new Datastore({ projectId: 'umass-plasma' });
+  let dsClientTrans = dsClient.transaction();
+
+  dsClientTrans.run(function() {
+    let transId = dsClient.key(['Transaction', req.body.transId]);
+
+    dsClientTrans.get(transId, function(err, trans) {
+      if(err || trans) {
+        dsClientTrans.rollback(function() { res.send('Invalid transaction ID.'); });
+      } else {
+        if(req.body.type === 'deposit') {
+          let acctNum = dsClient.key(['Account', req.body.to]);
+
+          dsClientTrans.get(acctNum, function(err, acct) {
+            acct.Balance += req.body.amount;
+            dsClientTrans.save({ key: acctNum, data: acct });
+            dsClientTrans.save({ key: transId, data: {} });
+            dsClientTrans.commit(function() { res.send('Deposit complete.'); });
+          });
+        } else if(req.body.type === 'transfer') {
+          let amnt = req.body.amount;
+          let acctNumFrom = dsClient.key(['Account', req.body.from]);
+          let acctNumTo = dsClient.key(['Account', req.body.to]);
+
+          dsClientTrans.get([acctNumFrom, acctNumTo], function(err, accts) {
+            if(accts[0].Balance >= amnt) {
+              accts[0].Balance -= amnt;
+              accts[1].Balance += amnt;
+              dsClientTrans.save([{ key: acctNumFrom, data: accts[0] },
+                { key: acctNumTo, data: accts[1] }]);
+              dsClientTrans.save({ key: transId, data: {} });
+              dsClientTrans.commit(function() { res.send('Transfer complete.'); });
+            } else {
+              dsClientTrans.rollback(function() { res.send('Insufficient funds.'); });
+            }
+          });
+        } else {
+          dsClientTrans.rollback(function() {
+            res.send('Unknown operation: ' + req.body.type);
+          });
+        }
+      }
+    });
+  });
+};
